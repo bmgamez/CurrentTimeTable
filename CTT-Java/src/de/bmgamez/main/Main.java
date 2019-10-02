ec9a80555503f0afc38a8d26249c55138deff18a
Benegamz <benegamz@t-online.de>
2019-10-02 16:33:26 +0200

    First Version

--- CTT-Java/src/de/bmgamez/main/Main.java
+++ CTT-Java/src/de/bmgamez/main/Main.java
@@ -1,12 +1,33 @@
 package de.bmgamez.main;
 
+import org.apache.pdfbox.io.RandomAccessRead;
 import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
+import java.io.File;
 import java.io.FileInputStream;
+
+import static org.apache.pdfbox.pdmodel.PDDocument.load;
 
 public class Main {
 
     public static void main(String[] args) {
-        PDFParser pdfParser = new PDFParser(new FileInputStream())
+
+        String string = null;
+        try {
+
+            File file = new File("resources/plan.pdf");
+
+            PDDocument pdDocument = PDDocument.load(file);
+            PDFTextStripper pdfTextStripper = new PDFTextStripper();
+
+            string = pdfTextStripper.getText(pdDocument);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Text:" + string);
     }
 }
