ca984c74c2a3e1ac2f8177222f131f62a005579a
Benegamz <benegamz@t-online.de>
2019-10-02 18:55:10 +0200

    Update Main.java

--- CTT-Java/src/de/bmgamez/ui/Main.java
+++ CTT-Java/src/de/bmgamez/ui/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("CCT.fxml"));
+        primaryStage.setTitle("VeryAcuratePlan");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
