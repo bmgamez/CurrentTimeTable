26aa50832a164557949340162f0725cfd2edfc41
Benegamz <benegamz@t-online.de>
2019-10-02 15:57:19 +0200

    Things

--- CTT-Java/src/de/bmgamez/main/Main.java
+++ CTT-Java/src/de/bmgamez/main/Main.java
@@ -1,8 +1,12 @@
 package de.bmgamez.main;
 
+import org.apache.pdfbox.pdfparser.PDFParser;
+
+import java.io.FileInputStream;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        PDFParser pdfParser = new PDFParser(new FileInputStream())
     }
 }
