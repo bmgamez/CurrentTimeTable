43ed00c8f72412738b05210b600df1c97b12ff74
Benegamz <benegamz@t-online.de>
2019-10-02 16:46:52 +0200

    Update Main.java

--- CTT-Java/src/de/bmgamez/main/Main.java
+++ CTT-Java/src/de/bmgamez/main/Main.java
@@ -1,20 +1,18 @@
 package de.bmgamez.main;
 
-import org.apache.pdfbox.io.RandomAccessRead;
-import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.File;
-import java.io.FileInputStream;
-
-import static org.apache.pdfbox.pdmodel.PDDocument.load;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        String klasse = "10d";
+
         String string = null;
+        String string1 = "";
         try {
 
             File file = new File("resources/plan.pdf");
@@ -23,11 +21,18 @@ public class Main {
             PDFTextStripper pdfTextStripper = new PDFTextStripper();
 
             string = pdfTextStripper.getText(pdDocument);
+            String[] lines = string.split("\r\n|\r|\n");
+
+            for (String line:lines) {
+                if (line.contains(klasse)) {
+                    string1 = string1 + line + "\n";
+                }
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        System.out.println("Text:" + string);
+        System.out.println(string1);
     }
 }
