42e24871924b654d03176003a319ec8e3cf8ce87
Benegamz <benegamz@t-online.de>
2019-10-03 15:58:48 +0200

    Fixed it again

--- CTT-Java/src/de/bmgamez/main/Main.java
+++ CTT-Java/src/de/bmgamez/main/Main.java
@@ -1,6 +1,5 @@
 package de.bmgamez.main;
 
-import de.bmgamez.backend.Downloader;
 import de.bmgamez.backend.Getter;
 import de.bmgamez.backend.Reader;
 import de.bmgamez.backend.Sorter;
@@ -22,10 +21,10 @@ public class Main {
         System.out.println(Arrays.toString(sorter.getLesson(reader.getOnlinePlan(Getter.get("path")), Getter.get("day"), 5)));
 
         for (int i = 1; i <= 11; i++) {
-            if (sorter.getLesson(Downloader.downloadPlan(Getter.get("day")), Getter.get("day"), i)[0] == null) {
+            if (sorter.getLesson(reader.getOnlinePlan(Getter.get("path")), Getter.get("day"), i)[0] == null) {
                 System.out.println(reader.getPlan("resources/plan.CSV", Getter.get("day"), i));
             } else {
-                System.out.println(sorter.getLesson(Downloader.downloadPlan(Getter.get("day")), Getter.get("day"), i)[1]);
+                System.out.println(sorter.getLesson(reader.getOnlinePlan(Getter.get("path")), Getter.get("day"), i)[1]);
             }
         }
 
