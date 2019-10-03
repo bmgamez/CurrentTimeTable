0c6e6019bd19a7f8b07148fdcf0fc664eaf8fbab
Manugamez <manuel@pielka.de>
2019-10-03 11:38:33 +0200

    Update Entry.java

--- CTT-Java/src/de/bmgamez/main/Entry.java
+++ CTT-Java/src/de/bmgamez/main/Entry.java
@@ -76,6 +76,7 @@ public class Entry {
             currentEntry.text = splitData[currentIndex];
             //currentIndex++;
         }
+        /*
         System.out.println(currentEntry.klassen[0]);
         System.out.println(currentEntry.klassen[1]);
         System.out.println(currentEntry.stunde);
@@ -85,6 +86,7 @@ public class Entry {
         System.out.println(currentEntry.neuerRaum);
         System.out.println(currentEntry.verlegungsdaten);
         System.out.println(currentEntry.text);
+        */
         return currentEntry;
     }
 }
