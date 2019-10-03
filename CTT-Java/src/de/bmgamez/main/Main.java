0e8a8eb795def2cd1eef430526989eca2e0cf29d
Benedict Weis <bene.we@t-online.de>
2019-10-03 13:00:07 +0200

    moved Reader

--- CTT-Java/src/de/bmgamez/main/Main.java
+++ CTT-Java/src/de/bmgamez/main/Main.java
@@ -2,6 +2,7 @@ package de.bmgamez.main;
 
 import de.bmgamez.backend.Downloader;
 import de.bmgamez.backend.Getter;
+import de.bmgamez.backend.Reader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
@@ -12,50 +13,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(searchPlan(Downloader.downloadPlan(Getter.get("day"))));
+        Reader reader = new Reader();
+
+        System.out.println(reader.searchPlan(Downloader.downloadPlan(Getter.get("day"))));
 
     }
 
-    public static String searchPlan(String destination) {
 
-        String string = null;
-        String string1 = "";
-
-        try {
-
-            File file = new File(destination);
-
-            PDDocument pdDocument = PDDocument.load(file);
-            PDFTextStripper pdfTextStripper = new PDFTextStripper();
-
-            string = pdfTextStripper.getText(pdDocument);
-            String[] lines = string.split("\r\n|\r|\n");
-
-            for (String line : lines) {
-                if (Getter.getArray("courses").toArray().length == 0) {
-                    if (line.contains(Getter.get("class"))) {
-                        string1 = string1 + line + "\n";
-                    }
-                } else {
-                    if (line.contains(Getter.get("class"))) {
-
-                        ArrayList<String> arrayList = Getter.getArray("courses");
-                        String[] courses = new String[arrayList.size()];
-                        courses = arrayList.toArray(courses);
-
-                        for (String course : courses) {
-                            if (line.contains(course)) {
-                                string1 = string1 + line + "\n";
-                            }
-                        }
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return (string1);
-    }
 }
