b305785c8f758901fdf5d95c6771be533a006785
Benegamz <benegamz@t-online.de>
2019-11-19 18:46:58 +0100

    Cleaned up the code (a bit)

--- CTT-Java/src/de/bmgamez/backend/Downloader.java
+++ CTT-Java/src/de/bmgamez/backend/Downloader.java
@@ -14,22 +14,15 @@ public class Downloader {
 
     public static String downloadPlan(int day) throws Exception {
 
-        URL url = new URL("https://schulinternes.de/dato40/server/hp-lazy-auth.php?data={%22schule%22:%22" + Getter.get("school") + "%22,%22user%22:%22" + Getter.get("username") + "%22,%22passwd%22:%22" + Getter.get("password") + "%22}");
-        URLConnection con = url.openConnection();
-        InputStream in = con.getInputStream();
-        String encoding = con.getContentEncoding();
-        encoding = encoding == null ? "UTF-8" : encoding;
-        String body = IOUtils.toString(in, encoding);
+        //Get the key for the authorization
+        String body = getBody("https://schulinternes.de/dato40/server/hp-lazy-auth.php?data={\"schule\":\"" + Getter.get("school") + "\",\"user\":\"" + Getter.get("username") + "\",\"passwd\":\"" + Getter.get("password") + "\"}", false);
 
-        URL url1 = new URL("https://schulinternes.de/dato40/server/hp-vertretungen-s.php?data=" + body);
-        URLConnection con1 = url1.openConnection();
-        InputStream in1 = con1.getInputStream();
-        String encoding1 = con1.getContentEncoding();
-        encoding1 = encoding1 == null ? "UTF-8" : encoding1;
-        String body1 = IOUtils.toString(in1, encoding1);
+        //Get the links for the PDF files
+        String body1 = getBody("https://schulinternes.de/dato40/server/hp-vertretungen-s.php?data=" + body, false);
 
-        String realurl = "https://schulinternes.de/dato40/server/hp-vertretungen-s.php?data=" + body;
-        Document doc = Jsoup.connect(realurl).get();
+        //Get the final link to download the PDF
+        String realUrl = "https://schulinternes.de/dato40/server/hp-vertretungen-s.php?data=" + body;
+        Document doc = Jsoup.connect(realUrl).get();
         Elements links = doc.select("a[href]");
 
         String finalURL = links.get(day).attr("abs:href");
@@ -37,22 +30,51 @@ public class Downloader {
         String path = Getter.get("path");
 
         try {
-            URL url23 = new URL(finalURL);
-            InputStream in23 = url23.openStream();
-            FileOutputStream fos23 = new FileOutputStream(path);
+
+            // Download the final PDF file
+            URL downloadUrl = new URL(finalURL);
+            InputStream downloadIn = downloadUrl.openStream();
+            FileOutputStream downloadFos = new FileOutputStream(path);
 
             int length = -1;
             byte[] buffer = new byte[1024];
-            while ((length = in23.read(buffer)) > -1) {
-                fos23.write(buffer, 0, length);
+            while ((length = downloadIn.read(buffer)) > -1) {
+                downloadFos.write(buffer, 0, length);
             }
 
-            fos23.close();
-            in23.close();
+            downloadFos.close();
+            downloadIn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return path;
+    }
+
+    public static String getBody(String stringUrl, boolean verbose) {
+
+        String body = null;
+
+        try {
+
+            if (verbose) System.out.println(stringUrl);
+
+            URL url = new URL(stringUrl);
+            URLConnection con = url.openConnection();
+            InputStream in = con.getInputStream();
+            String encoding = con.getContentEncoding();
+            encoding = encoding == null ? "UTF-8" : encoding;
+            body = IOUtils.toString(in, encoding);
+
+            if (verbose) System.out.println(body);
+
+            in.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return "false";
+        }
+
+        return body;
     }
 }
