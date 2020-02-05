668c856ea89241d6e9204b7c4da85475f089f1ca
Benegamz <benegamz@t-online.de>
2020-02-05 19:12:19 +0100

    did things

--- CTT_Final/PC/CTT_electron/download.js
+++ CTT_Final/PC/CTT_electron/download.js
@@ -1,4 +1,4 @@
-function getDownloadPage(day,callback) {
+function getDownloadPage(day, callback) {
 
     var settings = require("./resources/settings.json");
 
@@ -7,35 +7,33 @@ function getDownloadPage(day,callback) {
     request({
         uri: "https://schulinternes.de/dato40/server/hp-lazy-auth.php?data={\"schule\":\"" + settings.school + "\",\"user\":\"" + settings.username + "\",\"passwd\":\"" + settings.password + "\"}",
     }, function (error, response, body) {
-        
+
         request({
             uri: "https://schulinternes.de/dato40/server/hp-vertretungen-s.php?data=" + body,
         }, function (error, response, body) {
-            
+
             var cheerio = require('cheerio');
             var $ = cheerio.load(body);
-        
+
             var links = []
-        
+
             $($('a')).each(function (i, link) {
                 current = $(link).attr('href');
-                current = current.replace("./hp-show.php?data=","");
-                current = "https://schulinternes.de/dato40/server/hp-show.php?data=" + current;
+                current = current.replace("./hp-show.php?data=", "");
+                current = "http://schulinternes.de/dato40/server/hp-show.php?data=" + current;
 
                 links.push(current);
             });
 
 
-            var d=new Date();
-            if(d.getDay() > 5 && d.getHours() > 9) {
+            var d = new Date();
+            if (d.getDay() > 5 && d.getHours() > 9) {
                 if (day == 0) {
                     callback(links[0]);
+                } else if (day == 1) {
+                    callback(links[1]);
                 }
-                else if (day == 1){
-                    callback (links[1]);
-                }
-            }
-            else {
+            } else {
                 callback(links[day]);
             }
         });
@@ -43,6 +41,6 @@ function getDownloadPage(day,callback) {
 
 }
 
-getDownloadPage(1,(url) =>{
-    console.log(url)
+getDownloadPage(1, (url) => {
+    console.log(url);
 });
